06160e368fa306a12d1ee6d7e04608c8e2d04a9a
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-11-13 19:28:37 +0100

    feat/creacion de MainActivity, activity_main.xml y Dockerfile sin modificar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.feedback4_142514"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.feedback4_142514"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/feedback4_142514/MainActivity.kt
+++ app/src/main/java/com/example/feedback4_142514/MainActivity.kt
@@ -1,47 +1,47 @@
 package com.example.feedback4_142514
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.feedback4_142514.ui.theme.Feedback4_142514Theme
+import com.example.feedback4_142514.R
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Feedback4_142514Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        val buttonStart = findViewById<Button>(R.id.buttonStart)
+
+        // Cambiar el saludo según la hora del día
+        val currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val greeting = when {
+            currentTime in 0..11 -> "Buenos días"
+            currentTime in 12..17 -> "Buenas tardes"
+            else -> "Buenas noches"
         }
+        greetingTextView.text = greeting
+
+        //Navegar a la principal
+        buttonStart.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Feedback4_142514Theme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Feedback4_142514Theme {
+            ("Android")
+        }
     }
 }
